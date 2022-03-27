04c438875597b766efb9e4788f7d74ac48e7d408
lex <alex@bighill.org>
2022-03-26 21:32:23 -0700

    Tweak.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080;
 
 app.get("/:message", (req, res) => {
-  return res.json({ message: req.params.message + " !!!" });
+  return res.json({ message: req.params.message });
 });
 
 app.get("/", (req, res) => {
